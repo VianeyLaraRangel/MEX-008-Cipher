8863495981f4dab55b84b7cb20c499a484bb9e1e
Vianey Lara Rangel <vianey.lara.rangel@gmail.com>
2019-06-12 21:48:00 -0500

    Pasando como parámetros al objeto window.cipher, los elementos del DOM

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 
-/*Aquí debes escuchar eventos del DOM, invocar cipher.encode() o cipher.decode() según sea necesario y actualizar el resultado en la UI.*/
+/*LABORATORIA: Aquí debes escuchar eventos del DOM, invocar cipher.encode() o cipher.decode() según sea necesario y actualizar el resultado en la UI.*/
 
-//Obtener número de desplazamiento (offset o key) y guardárlo en una varible
-//let offset = document.getElementById('offset-box').value;
-//console.log('offset');
+//PASO 1: Obtener número de desplazamiento (offset/key/llave), guardárlo en una varible y convertirlo a entero.
 
-//console.log('offset-box');
+let offset = parseInt(document.getElementById('offset-box').value);
+//console.log(key);
 
-//Detonar el evento del input de desplazamiento (offset o key) y pasarle la función que convierte el offset en un entero.
-//offset.addEventListener('click', offsetParse());
+//PASO 2: Obtener la cadena/string del texto a cifrar y convertirlo a mayúscula
+let string = document.getElementById('decodex-string').value.toUpperCase();
+//console.log(codexString);
 
+
+
+
+
+
+//Conocer la posición del código ASCII para desplazar
+// codexString.charCodeAt(0);
+// console.log(codexString);
+
+//Detonar el evento del input de desplazamiento (offset o key)
+//offsetKey.addEventListener('click', window.cipher.encode());
+
+
+//Invoca la data que obtuvimos
+//cipherData();
+//Aquí invoco las funciones
+//window.cipher.encode();
+
+
+//Hacer una función que obtenga los datos del DOM
+//const cipherData = (codexOrDecodex) => {}
