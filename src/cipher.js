db0b76b16388fcaa94aaf31d9055955830aa8339
Vianey Lara Rangel <vianey.lara.rangel@gmail.com>
2019-06-13 16:20:18 -0500

    Corrección del scope y del addEventListener con apoyo de Silvana

--- src/cipher.js
+++ src/cipher.js
@@ -1,19 +1,19 @@
-/*LABORATORIA:Implementar el objeto cipher, el cual debe estar exportado en el objeto global (window). Este objeto (cipher) debe contener dos métodos: cipher.encode(offset, string): y cipher.decode(offset, string):
+/*LABORATORIA:Implementar el objeto cipher, el cual debe estar exportado en el objeto global (window). Este objeto (cipher) debe contener dos métodos: cipher.encode(offset, string): y cipher.decode(offset, string): */
 
 window.cipher = {
-    //encode: (offset, string) => {
+    encode: (offset, string) => {
+        console.log (offset, string);
 //PASO 3: Iterar sobre la palabra para saber cuantas letras cifraremos
-//     for (let i= 0; i<(string.length); i++) {
-//     //PASO 3.1: Saber cuál es la posición ascci para c/letra que se recorre
-//         let asciiPosition = string.charCodeAt(i);
-//         //console.log('asciiPosition');
+    //for (let i= 0; i<(string.length); i++) {
+//PASO 3.1: Saber cuál es la posición ascci para c/letra que se recorre
+        //let asciiPosition = string.charCodeAt(i);
+        //console.log('asciiPosition');
     
-//     /*PASO 3.2: Aplicar fórmula al texto ascci para saber cual es la nueva posición (x -65 + n) % 26 +65*/
-//         let enCodexString = (asciiPosition-65 + offset) %26 + 65;
+//PASO 3.2: Aplicar fórmula al texto ascci para saber cual es la nueva posición (x -65 + n) % 26 +65*
+        //let enCodexString = (asciiPosition-65 + offset) %26 + 65;
 
-//     //PASO 3.3: Teniendo la posición definitiva de cifrado, transformar a alfabeto y guardarlo en una variable
-//     let result = String.fromCharCode(enCodexString);
-//         }
-//         return result;
-//     }
+//PASO 3.3: Teniendo la posición definitiva de cifrado, transformar a alfabeto y guardarlo en una variable
+    //let result = String.fromCharCode(enCodexString);
+    }
+    //return (result);
 };
--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
 /*LABORATORIA: Aquí debes escuchar eventos del DOM, invocar cipher.encode() o cipher.decode() según sea necesario y actualizar el resultado en la UI.*/
 
-//Detonar eventos del DOM
-//offsetKey.addEventListener('click', window.cipher.encode());
+//Variables a utilizar
 
-//PASO 1: Obtener número de desplazamiento (offset/key/llave), guardárlo en una varible y convertirlo a entero (parseInt). (Pasarlo como parámetro al objeto window.cipher)
-const offsetKey = document.getElementById('offset-box').value;
-console.log('holaaaa');
-}
+let buttonCifrar = document.getElementById('button-cifrar');
+
+//Eventos del DOM
+buttonCifrar.addEventListener('click', () => {
+    let offsetKey = document.getElementById('offset-box');
+    let encodeString = document.getElementById('encode-string');
+    //console.log(encodeString.value);
+        offsetKey.value
+        window.cipher.encode(offsetKey.value, encodeString.value);
+});
+
+//PASO 1: Obtener offset (convertirlo a entero (parseInt)) y el string a cifrar, luego guardár la data en una varible.
+
+
+//console.log(offsetKey);
 
 //PASO 2: Obtener la cadena/string del texto a cifrar y convertirlo a mayúscula.(Pasarlo como parámetro al objeto window.cipher)
-// let string = document.getElementById('decodex-string').value;
-// //console.log(codexString);
 
 //PASO 4: Imprimir resultado en el input correspondiente
 //document.getElementById(encodex-string).value.innerHTML
 
-
-
-
-//Conocer la posición del código ASCII para desplazar
-// codexString.charCodeAt(0);
-// console.log(codexString);
-
 //Invoca las funciones que obtuvimos
-getData();
-// window.cipher.encode();
+
