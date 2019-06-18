6c2da7c252d1488320dc577d4afab944ada0400f
Vianey Lara Rangel <vianey.lara.rangel@gmail.com>
2019-06-18 18:09:56 -0500

    Cifrando mayusculas y minusculas

--- src/cipher.js
+++ src/cipher.js
@@ -1,27 +1,39 @@
 window.cipher = {
     encode: (offset, string) => {
-        let result = '';
+        let resultEncode = '';
         let offsetParsed = (parseInt(offset));
         //let stringMinusc = (string.toLowerCase());
 
         for (let i=0; i<string.length; i++) {
-            let stringMayusc = (string.toUpperCase());
-            let positionAscci = stringMayusc.charCodeAt(i);
-            let positionEncode =((positionAscci-65+offsetParsed)%26+65);
-            result += String.fromCharCode(positionEncode);
-            }
-            return (result);
+            console.log(string);
+            //let stringMayusc = (string.toUpperCase());
+            let positionAscci = string.charCodeAt(i);
+            console.log(positionAscci);
+            
+            if (positionAscci >= 65 && positionAscci <= 90) {
+                let positionEncode =((positionAscci-65+offsetParsed)%26+65);
+                console.log(positionEncode);
+                resultEncode += String.fromCharCode(positionEncode);
+            }else if (positionAscci >= 97 && positionAscci <= 122){
+                let positionEncode = ((positionAscci-97+offsetParsed)%26+97);
+                console.log(positionEncode);
+                resultEncode += String.fromCharCode(positionEncode);
+            } else {
+                console.log('error');   
+                }   
+        }
+    return (resultEncode);
     },
+
     decode: (offset, string) => {
         let resultDecode = '';
         let offsetParsed = (parseInt(offset));
-
-        for (let i=0; i<string.length; i++) {
+        for (let i = 0; i < string.length; i++) {
             let stringMayusc = (string.toUpperCase());
             let positionAscci = stringMayusc.charCodeAt(i);
-            let positionDecode = ((positionAscci+65-offsetParsed)%26+65);
+            let positionDecode = ((positionAscci + 65 - offsetParsed) % 26 + 65);
             resultDecode += String.fromCharCode(positionDecode);
-            }
-        return (resultDecode);
+        }
+    return (resultDecode);
     }
 };
