6c91fd4753ccf34a8a18090e762b6050593f48ab
Vianey Lara Rangel <vianey.lara.rangel@gmail.com>
2019-06-18 17:04:49 -0500

    Modificando scope de variable en dónde se transforma a mayusculas, se coloca dentro del bucle para que solo viva ahí

--- src/cipher.js
+++ src/cipher.js
@@ -1,20 +1,23 @@
 window.cipher = {
     encode: (offset, string) => {
         let result = '';
-        let stringMayusc = (string.toUpperCase());
         let offsetParsed = (parseInt(offset));
-        for (let i=0; i<stringMayusc.length; i++) {
+        //let stringMinusc = (string.toLowerCase());
+
+        for (let i=0; i<string.length; i++) {
+            let stringMayusc = (string.toUpperCase());
             let positionAscci = stringMayusc.charCodeAt(i);
             let positionEncode =((positionAscci-65+offsetParsed)%26+65);
             result += String.fromCharCode(positionEncode);
             }
-        return (result);
+            return (result);
     },
     decode: (offset, string) => {
         let resultDecode = '';
-        let stringMayusc = (string.toUpperCase());
         let offsetParsed = (parseInt(offset));
-        for (let i=0; i<stringMayusc.length; i++) {
+
+        for (let i=0; i<string.length; i++) {
+            let stringMayusc = (string.toUpperCase());
             let positionAscci = stringMayusc.charCodeAt(i);
             let positionDecode = ((positionAscci+65-offsetParsed)%26+65);
             resultDecode += String.fromCharCode(positionDecode);
