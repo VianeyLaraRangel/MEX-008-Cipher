2e1adfb8884191ca42a224edb7567f2e7eabf7cc
Vianey Lara Rangel <vianey.lara.rangel@gmail.com>
2019-06-23 15:37:34 -0500

    Modificando valor de la resta y del modulo para que el descifrado de minusculas se mantenga en el rango positivo

--- src/cipher.js
+++ src/cipher.js
@@ -30,14 +30,20 @@ window.cipher = {
         for (let i = 0; i < string.length; i++) {
             let positionAscci = string.charCodeAt(i);
             if (positionAscci >= 65 && positionAscci <= 90) {
-                let positionDecode = ((positionAscci+65-offsetParsed) % 26 + 65);
-                resultDecode += String.fromCharCode(positionDecode);
+                let positionDecodeMayus = ((positionAscci+65-offsetParsed) % 26 + 65);
+                resultDecode += String.fromCharCode(positionDecodeMayus);
             } else if (positionAscci >= 97 && positionAscci <= 122) {
-                let positionDecode = ((positionAscci+97+offsetParsed) % 26 + 97);
-                resultDecode += String.fromCharCode(positionDecode);
-            } else if (positionAscci === 32 || positionAscci === 165 || positionAscci === 164) {
-                let positionDecode = positionAscci;
-                resultDecode += String.fromCharCode(positionDecode);
+                let positionDecodeMinus = ((positionAscci-19-offsetParsed) % 26 + 97);
+                resultDecode += String.fromCharCode(positionDecodeMinus);
+            } else if (positionAscci === 32) {
+                let positionDecodeSpace = positionAscci;
+                resultDecode += String.fromCharCode(positionDecodeSpace);
+            } else if (positionAscci >= 33 && positionAscci <= 64) {
+                let positionDecodeEspecial = ((positionAscci-33-offsetParsed)%32+33);
+                resultDecode += String.fromCharCode(positionDecodeEspecial);
+            } else if (positionAscci >= 161 && positionAscci <= 250) {
+                let positionDecodeEspecialComplete = ((positionAscci -161 -offsetParsed)%90 +161);
+                resultDecode += String.fromCharCode(positionDecodeEspecialComplete);
             }
         }
     return (resultDecode);
