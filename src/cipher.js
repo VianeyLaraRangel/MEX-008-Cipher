21481eda2f7e27eebeec03ffa383ebe420839189
Vianey Lara Rangel <vianey.lara.rangel@gmail.com>
2019-06-23 15:22:35 -0500

    Cambiando el valor del modulo en la formula de minúsculas y caracteres especiales, arreglando los números que se movía de mas el cifrado, por fin!

--- src/cipher.js
+++ src/cipher.js
@@ -5,15 +5,21 @@ window.cipher = {
         for (let i=0; i<string.length; i++) {
             let positionAscci = string.charCodeAt(i);            
             if (positionAscci >= 65 && positionAscci <= 90) {
-                let positionEncode =((positionAscci-65+offsetParsed)%26+65);
-                resultEncode += String.fromCharCode(positionEncode);
+                let positionEncodeMayus =((positionAscci-65+offsetParsed)%26+65);
+                resultEncode += String.fromCharCode(positionEncodeMayus);
             } else if (positionAscci >= 97 && positionAscci <= 122) {
-                let positionEncode = ((positionAscci-97+offsetParsed)%26+97);
-                resultEncode += String.fromCharCode(positionEncode);
-            } else if (positionAscci === 32 || positionAscci === 165 || positionAscci === 164) {
-                let positionEncode = positionAscci;
-                resultEncode += String.fromCharCode(positionEncode);
-            }   
+                let positionEncodeMinus = ((positionAscci-97+offsetParsed)%26+97);
+                resultEncode += String.fromCharCode(positionEncodeMinus);
+            } else if (positionAscci === 32) {
+                let positionEncodeSpace = positionAscci;
+                resultEncode += String.fromCharCode(positionEncodeSpace);
+            }   else if (positionAscci >= 33 && positionAscci <= 64) {
+                let positionEncodeEspecial = ((positionAscci-33+offsetParsed)%32+33);
+                resultEncode += String.fromCharCode(positionEncodeEspecial);
+            } else if (positionAscci >= 161 && positionAscci <= 250) {
+                let positionEncodeEspecialComplete = ((positionAscci -161 + offsetParsed)%90+161);
+                resultEncode += String.fromCharCode(positionEncodeEspecialComplete);
+            }
         }
     return (resultEncode);
     },
